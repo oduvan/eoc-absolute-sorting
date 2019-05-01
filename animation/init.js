c86200f2491dacac3356f8aecc19272073a3275e
Alexander Lyabah <a.lyabah@checkio.org>
2019-05-02 00:25:53 +0300

    first commit

---
+++ animation/init.js
@@ -0,0 +1,12 @@
+//Dont change it
+requirejs(['ext_editor_io', 'jquery_190', 'raphael_210'],
+    function (extIO, $, TableComponent) {
+        var io = new extIO({
+            functions: {
+                js: 'absoluteSorting',
+                python: 'checkio'
+            }
+        });
+        io.start();
+    }
+);
---
+++ verification/src/referee.py
@@ -0,0 +1,25 @@
+from checkio_referee import RefereeRank, ENV_NAME
+
+
+
+import settings_env
+from tests import TESTS
+
+
+class Referee(RefereeRank):
+    TESTS = TESTS
+    ENVIRONMENTS = settings_env.ENVIRONMENTS
+
+    DEFAULT_FUNCTION_NAME = "checkio"
+    FUNCTION_NAMES = {
+        "python_3": "checkio",
+        "js_node": "absoluteSorting"
+    }
+    ENV_COVERCODE = {
+        ENV_NAME.PYTHON: '''def cover(func, data):
+    result = func(tuple(data))
+    if not isinstance(result, (list, tuple)):
+        raise TypeError("The result should be a list or a tuple")
+    return result
+'''
+    }
